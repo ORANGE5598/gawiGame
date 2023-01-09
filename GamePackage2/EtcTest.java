af6b2c845ea5bcfa60765114f1767a37f1e8b13f
jay3614 <117256289+jay3614@users.noreply.github.com>
2023-01-09 17:33:35 +0900

    양식 통일+etc 완성본(깔끔하지 않은 디자인 버전)

--- GamePackage2/EtcTest.java
+++ GamePackage2/EtcTest.java
@@ -7,35 +7,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.NavigableMap;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.Map.Entry;
 
 import javax.swing.JOptionPane;
 
 public class EtcTest {
-	String path = "C:\\Users\\Manic-063\\git\\gawiGame\\userData";
-	File file = new File(path);
-
-	String nthLine, subLine;
-	String getId = null;
-	String maskId = null;
-	String input;
-	String winRate;
-	String userId;
+	String path = "C:\\userData";
+	String nthLine, subLine, maskId, input, winRate, userId;
 	int winRate2;
-	Path path2;
-	
-	ArrayList<String> al;
-	List<String> allLines = null;
-	String[] fList = file.list();
-	Entry<Double, String> entry = null;
 	double rate;
-
+	Path path2;
+	File file = new File(path);
+	String[] fList = file.list();
+	ArrayList<String> al;
+	List<String> allLines;
 	File inputFile = new File(path + "\\" + winRate + ".dat");
 
 	public void startMessage() {
@@ -81,7 +66,9 @@ public class EtcTest {
 //----------------------------------------------------------------------------------------------------
 
 	private void rateFirst() {	// 승률 1위 출력하는 메서드
-
+		
+		al = new ArrayList<String>();
+		
 		for (int i = 0; i < fList.length; i++) {	// 루프를 돌려 파일을 순차적으로 읽어내기
 			path2 = Paths.get(path + "\\" + fList[i]);	// 폴더 경로 + \\ + 파일이름으로 파일경로 지정
 
@@ -90,26 +77,16 @@ public class EtcTest {
 				nthLine = allLines.get(10);			// 파일 내용 전체 중 11 라인(첫 번째 줄이 0부터 시작)에 있는 winRate 줄
 				subLine = nthLine.substring(10, nthLine.length());	// 승률부분만 substring으로 잘라내기
 				rate = Double.parseDouble(subLine);		// 승률을 double로 변경
-				maskId = fList[i].substring(0, fList[i].length()-7) + "***";
+				maskId = fList[i].substring(0, fList[i].length()-7) + "***";	// id.dat의 뒤 7자리까지 잘라서 id뒷3자리 자른 후 *** 붙이기
 				al.add(rate + ":" + maskId);
-//				scores.put(rate, maskId);
 			} catch (IOException e) {
 				System.out.println("예외 발생");
 			}
-		}
-//		entry = scores.lastEntry();
-//		String first = entry.getValue();
-//		
-//		int length = first.length();
-//		length = length - 7;
-//		maskId = first.substring(0, length);
-//		maskId = maskId + "***";
-//		first = maskId;
+		}	// for문의 끝
 
-//		System.out.println(maskId);
 		Collections.sort(al, Collections.reverseOrder());	// ArrayList al을 내림차순으로 정렬
-		JOptionPane.showMessageDialog(null, al);	// 정렬한 내용을 출력
-	}
+		JOptionPane.showMessageDialog(null, al.get(0).substring(al.get(0).indexOf(':')+1));	// 1등(0번 index)의 이름만 subString으로 잘라서 출력
+	}	// rateFirst()의 끝
 
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 	private void playerArrays() {	// 승률 오름차순으로 꼴등부터 출력하는 메서드
---
+++ GamePackage2/GawibawiboMain.java
@@ -0,0 +1,87 @@
+package GamePackage2;
+
+import javax.swing.JOptionPane;
+
+public class GawibawiboMain {
+	static private String userInput; // 필드 선언
+	static private int gameResult;
+	
+	static private String email;
+	static private String password;
+	static private boolean flag = false;
+	
+	static private PlayerDAO dao; // DAO 에서 대부분의 작업을 수행할 예정임.
+	static private PlayerInfo dto; // 사용자의 모든 데이터는 DTO를 사용하여 수행.
+	static private GameLogic logic; // 실제 가위바위보 로직 수행
+	
+	
+	////////////////// 이하 객체들은 임시로 생성한 것
+	static private Register register; // 임시
+	static private changePW changepw; // 임시
+//	static private Etc etc;
+	static private EtcTest etc;
+	
+	
+
+	
+	public static void main(String[] args) {
+		startMenu();
+
+	}
+	
+	public static void startMenu() { // 첫 시작시 호출되는 메서드 startMenu()
+		
+		userInput = JOptionPane.showInputDialog("1. 로그인하기 2. 사용자 계정 생성 3. 기타 메뉴 보기"); // 사용자에게 입력 받기
+		if(userInput.equals("1")) { // 로그인일 경우
+			login();
+		} else if(userInput.equals("2")) { // 계정 생성일 경우
+			register = new Register();
+			register.inputEmail();
+		} else if(userInput.equals("3")) {
+			etc = new EtcTest();
+			etc.startMessage();
+//			etc = new Etc();
+//			etc.startMessage();
+			
+		} else { // 1 2 3 모두 아닐 경우 재입력 요구
+			JOptionPane.showMessageDialog(null, "잘못 입력했습니다. 다시 시도하세요.");
+			startMenu();
+		}
+	
+	}
+	
+	public static void login() {
+		email = JOptionPane.showInputDialog("이메일을 입력하세요.");
+		password = JOptionPane.showInputDialog("패스워드를 입력하세요.");
+		dto = new PlayerInfo(System.currentTimeMillis(), email, password);
+		
+		System.out.println(dto.getEmail());
+		System.out.println(dto.getPassword());
+		new PlayerDAO().check(dto);
+		
+		
+		
+		
+	}
+	
+	public static void afterLogin() {
+		dao = new PlayerDAO();
+		userInput = JOptionPane.showInputDialog("1. 게임시작 2. 전적 보기 3. 마지막 로그인 날짜 확인 4. 암호 변경");
+		
+		if(userInput.equals("1") ) {
+			logic = new GameLogic();
+			gameResult = logic.getResult(); // 게임 결과값 받아옴
+			
+		} else if(userInput.equals("2")) {
+			// 전적 보여주기
+		} else if(userInput.equals("3")) {
+			// 마지막 로그인 날짜
+		} else if(userInput.equals("4")) {
+			// 암호 변경 
+			changepw = new changePW();
+			changepw.inputPW();
+		}
+		
+	}
+
+}
